bf8d214901850a9bf7709a46b9d3e72380763294
mkulinski <mkulinski@snapchat.com>
2023-12-11 08:08:59 -0800

    Finished day 2

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/mikekulinski/advent-of-code/day1"
+	"github.com/mikekulinski/advent-of-code/day2"
 	"github.com/mikekulinski/advent-of-code/file_reader"
+	"log"
 )
 
 func main() {
-	lines := file_reader.ReadFile("day1/input.txt")
+	lines := file_reader.ReadFile("day2/input.txt")
 
-	total, err := day1.Part2(lines)
+	total, err := day2.Part2(lines)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Println(total)
 }
